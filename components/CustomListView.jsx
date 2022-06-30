1acb7fc7a83b37ffc911847275485dcecb97f97b
Josh <j.p.lu@outlook.com>
2022-06-30 11:26:55 +1000

    feat: add list concept

---
+++ components/CustomListView.jsx
@@ -0,0 +1,15 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const CustomListView = () => {
+  return (
+    <ListGroup className="questions-list">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+  );
+};
+
+export default CustomListView;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomNavbar from '../components/CustomNavbar';
+import CustomListView from '../components/CustomListView';
 
 const Homepage = () => {
-  return <CustomNavbar />;
+  return (
+    <>
+      <CustomNavbar />
+      <CustomListView />
+    </>
+  );
 };
 
 export default Homepage;
