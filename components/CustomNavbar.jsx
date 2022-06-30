cec29e7b9fcdcc6b3df6e8f779eca89b15fa3933
Josh <j.p.lu@outlook.com>
2022-07-01 02:35:29 +1000

    fix: navbar home links

--- components/CustomNavbar.jsx
+++ components/CustomNavbar.jsx
@@ -7,11 +7,11 @@ const CustomNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">MyQ&A</Navbar.Brand>
+        <Navbar.Brand href="/">MyQ&A</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">About</Nav.Link>
             <NavDropdown title="Departments" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Team A</NavDropdown.Item>
