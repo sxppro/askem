c5cf540a8148b647e7f08797a20e129ad9759c71
Rishi <rishipatel3012@gmail.com>
2022-06-30 14:47:05 +1000

    Feat: New File for individual pages

---
+++ pages/QuestionOne.js
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+
+const QuestionOne = () => {
+    return (
+      <>
+        <Container>
+            Rishi
+        </Container>
+      </>
+    );
+};
+
+export default QuestionOne;
