43baa108b9077e2b94cb7107cd87a6c370879c62
Josh <j.p.lu@outlook.com>
2022-07-01 10:31:56 +1000

    fix: cleanup and set site title + icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.css';
 import { ApolloProvider } from '@apollo/client';
+import Head from 'next/head';
 import SSRProvider from 'react-bootstrap/SSRProvider';
 import client from '../utils/graphql';
 
@@ -8,6 +9,10 @@ const Homepage = ({ Component, pageProps }) => {
   return (
     <ApolloProvider client={client}>
       <SSRProvider>
+        <Head>
+          <title>Ask&apos;em</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
       </SSRProvider>
     </ApolloProvider>
