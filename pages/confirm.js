f0b01fc0409167b61253c34df88ed58625de4875
Josh <j.p.lu@outlook.com>
2022-11-20 15:44:44 +1100

    feat: update confirmation page

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react';
 import { Center, VStack, Heading, Spinner } from '@chakra-ui/react';
 import { confirmUser } from '../utils/app';
 
 const ConfirmationPage = () => {
+  const [fade, setFade] = useState({ fade: 'fade-in' });
+
+  useEffect(() => {
+    const fadeTimeout = setInterval(() => {
+      fade.fade === 'fade-in'
+        ? setFade({ fade: 'fade-out' })
+        : setFade({ fade: 'fade-in' });
+    }, 750);
+
+    return () => clearInterval(fadeTimeout);
+  }, [fade]);
+
   return (
     <Center h={'calc(100vh - 64px)'} onLoad>
       <VStack spacing={8}>
-        <Heading size="lg">Confirming ...</Heading>
+        <Heading
+          size="lg"
+          transition={'opacity 1.5s ease'}
+          opacity={fade.fade === 'fade-out' ? 0.3 : 1}
+        >
+          Confirming
+        </Heading>
         <Spinner size="xl" />
       </VStack>
     </Center>
@@ -18,17 +37,18 @@ export async function getServerSideProps(context) {
     const { token, tokenId } = query;
     await confirmUser(token, tokenId);
   };
+  let isValidated = false;
 
   try {
     await confirmEmail();
+    isValidated = true;
   } catch (err) {
     console.error(err);
   }
 
   return {
-    redirect: {
-      destination: '/login',
-      permanent: true,
+    props: {
+      isValidated,
     },
   };
 }
